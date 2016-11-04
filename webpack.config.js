130f8da3f642a308fd79ea42032b3d617017bc25
Sergio Fernandez <sergio@kasfactory.net>
2016-11-04 13:29:17 +0100

    Added styles!

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,32 @@
 var webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-	entry: './src/main.ts',
+	entry: ['./src/main.ts'],
 	output: {
 		path: './dist',
 		filename: 'app.bundle.js'
 	},
 	module: {
 		loaders: [
-			{test:/\.ts$/, loader: 'ts-loader'}
+			{
+				test:/\.ts$/,
+				 loader: 'ts-loader'
+			},
+			{
+                test: /\.css$/,
+                loader: ExtractTextPlugin.extract('style', 'css?sourceMap')
+            }
 		]
 	},
 	resolve: {
-		extensions: ['', '.js', '.ts']
+		extensions: ['', '.js', '.ts', '.css']
 	},
 	plugins: [
 		new HtmlWebpackPlugin({
 			template: './src/index.html'
-		})
+		}),
+		new ExtractTextPlugin('./dist/style.css')
 	]
 };
